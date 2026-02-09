c42cd48de004c82a6e0105f18f2e690239cc48d1
Kamlesh-Wamankar-BAU-Kyndryl <kamlesh.wamankar@kyndryl.com>
2026-02-09 14:45:38 +0530

    Variable updated

--- variable.tf
+++ variable.tf
@@ -1,23 +1,7 @@
 //create variable for resource group name, location and ubuntu vm name
-variable "resource_group_name" {
-  description = "The name of the resource group"
-  type        = string
-}
 
-variable "location" {
-  description = "The location of the resource group"
-  type        = string
-}
-
-variable "ubuntu_vm_name" {
-  description = "The name of the Ubuntu VM"
-  type        = string
-}
-variable "vnet_name" {
-  description = "The name of the Vnet"
-    type        = string
-}
-variable "subscription_id" {
-  description = "The subscription ID for Azure"
-  type        = string
-}
+variable "resource_group_name" {}
+variable "location" {}
+variable "ubuntu_vm_name" {}
+variable "vnet_name" {}
+variable "subscription_id" {}
